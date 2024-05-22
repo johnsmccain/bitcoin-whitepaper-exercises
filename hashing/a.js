5e01ec6a83e8ee263cc658a511dd4b8f7c8a55ab
johnsmccain <johnsdanlami@gmail.com>
2024-05-22 16:05:28 +0100

    Hashing excersis

---
+++ hashing/a.js
@@ -0,0 +1,9 @@
+let a = "00000";
+let b = "";
+
+
+if (a){
+	console.log(true);
+} else{
+	console.log(false);
+}
--- hashing/hashing.js
+++ hashing/hashing.js
@@ -19,6 +19,7 @@ var Blockchain = {
 	blocks: [],
 };
 
+
 // Genesis block
 Blockchain.blocks.push({
 	index: 0,
@@ -28,16 +29,69 @@ Blockchain.blocks.push({
 });
 
 // TODO: insert each line into blockchain
-// for (let line of poem) {
-// }
+for (let line of poem) {
+	createBlock(line);
+}
 
-// console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
+function createBlock(msg) {
+	let currentIndex = Blockchain.blocks.length;
+	let block = {
+		index: currentIndex,
+		prevHash: Blockchain.blocks[currentIndex -1].hash,
+		data: msg,
+		timestamp: Date.now(),
+	}
+	block.hash = blockHash(block);
+	Blockchain.blocks.push(block);
+	return block;
+}
+
+for (let block of Blockchain.blocks){
+	console.log(block)
+}
+
+console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
 
 
 // **********************************
 
+function verifyChain (Blockchain) {
+
+	let genesis = 0;
+	for (let block of Blockchain.blocks){
+		if (block.hash === "000000" && block.index === 0){
+			genesis = 1;
+		}
+		if (genesis === 1){
+			for (let block of Blockchain.blocks){
+
+				verifyBlock(block);
+			}
+		} else {
+			return false;
+		}
+	}
+	return true
+}
+
+
+function verifyBlock(block){
+	if (block.data && block.hash && block.index >= 0){
+		let blockData = JSON.stringify(block);
+		let currentHashedBlock = crypto.createHash("sha256").update(blockData).digest("hex");
+		if (block.hash !== currentHashedBlock){
+			return false;
+		}
+		console.log(`${block.hash} is verified`)
+	}
+
+	return false;
+}
+
 function blockHash(bl) {
+	let blockData = JSON.stringify(bl)
 	return crypto.createHash("sha256").update(
 		// TODO: use block data to calculate hash
+		blockData
 	).digest("hex");
 }
